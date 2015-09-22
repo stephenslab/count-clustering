a07f288ec650c3ad99e9c0db99d56a79472f1e9c
Kushal K Dey <kshldey@gmail.com>
2015-09-22 12:17:47 -0500

    modified the test code

--- test/test_code.R
+++ test/test_code.R
@@ -1,7 +1,10 @@
 
 library(maptpx)
-test_data <- read.table('../data/test_data.txt')
+test_data <- read.table('test_data.txt')
+head(test_data)
 out <- topics(test_data,K=4,tol=0.01);
 
+## error message in the Quasi Newton acceleration
 
-## gives error message
+
+
