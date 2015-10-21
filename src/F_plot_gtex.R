e5e9ce87aad818227a255ca1605bee7549d1f28c
Kushal K Dey <kshldey@gmail.com>
2015-10-20 19:41:18 -0500

    made changes to docs- paper updated

--- src/F_plot_gtex.R
+++ src/F_plot_gtex.R
@@ -2,11 +2,11 @@
 
 ## plot hierarchical and admixture F for GTEX data
 setwd("/Users/kushal/Documents/count-clustering/src/")
-hierarchy_F <- read.table("../internal_data/hierarchy_F_thin_1.txt")
-admixture_F <- read.table("../internal_data/admixture_F_thin_1.txt")
+#hierarchy_F <- read.table("../internal_data/hierarchy_F_thin_0.txt")
+#admixture_F <- read.table("../internal_data/admixture_F_thin_0.txt")
 
-hierarchy_F[hierarchy_F < 1] = 0;
-admixture_F[admixture_F < 1] = 0
+hierarchy_F <- read.table("../internal_data/hierarchy_F_clus_5_thin_2.txt")
+admixture_F <- read.table("../internal_data/admixture_F_clus_5_thin_2.txt")
 
 hierarchy_F <- hierarchy_F[1:48,1:48];
 admixture_F <- admixture_F[1:48,1:48];
@@ -20,6 +20,8 @@ for(i in 1:dim(hierarchy_F)[1])
   }
 }
 
+hierarchy_F[hierarchy_F < 0.8] = 0;
+admixture_F[admixture_F < 0.8] = 0;
 
 heatmap(as.matrix(hierarchy_F))
 heatmap(as.matrix(admixture_F))
@@ -37,11 +39,11 @@ rownames(admixture_F) <- unique_tissues
 
 library(cape)
 
-png(filename="../plots/hierarchy_F_thin_1.png", res=100)
+png(filename="../plots/hierarchy_F_thin_clus_5_1.png", res=100)
 myImagePlot(as.matrix(hierarchy_F))
 dev.off()
 
-png(filename="../plots/admixture_F_thin_1.png", res=100)
+png(filename="../plots/admixture_F_thin_clus_5_1.png", res=100)
 myImagePlot(as.matrix(admixture_F))
 dev.off()
 
