7cd2f1944a9d8c6fbecdc8f89fb7eb342494642a
jhsiao999 <joyce.hsiao1@gmail.com>
2016-02-24 20:34:33 -0600

    add GTEx code for plotting

--- project/R/paper-figures.R
+++ project/R/paper-figures.R
@@ -1,22 +1,179 @@
 ###--- GTEx ---###
+omega <- read.table("../project/rdas/omega_cis_genes_0_1_2.txt",
+                    header = TRUE, sep = " ",
+                    stringsAsFactors = FALSE)
+dim(omega)
+head(omega)
+colnames(omega) <- c(1:NCOL(omega))
 
-sample_labels <- c("Adipose Subcutaneous: 111", "Adipose Visceral: 22", "Adrenal Gland: 47", 
-                    "Artery Aorta: 74", "Artery Coronary: 40", "Artery Tibial: 124", 
-                    "Blood: 168", "Brain Amygdala: 26", "Brain Caudate Basal Ganglia: ", 
-                    "Brain Cerebellar Hemisphere", "Brain Cerebellum: 29",
-                    "Brain Cortex: 24", "Brain Frontal Cortex: 27",
-                    "Brain Hypthallamus: 28", "Brain Nucleus Accumbens: 30",
-                    "Brain Putamen: 23", "Brain Substantia Nigr: 26", "Breast: 57",
-                    "Cell Line EBV Lymphocytes: 42", "Colon Transverse: 53",
-                    "Esophagus Mucosa: 95", "Esophagus MUscularis: 91",
-                    "Heart Atrial Appendage: 27", "Heart Left Ventrical: 87",
-                    "Liver: 32", "Lung: 124", "Muscle Skeletal: 143",
-                    "Nerve Tibial: 102", "Ovary: 30", "Pancreas: 58",
-                    "Pituitary: 22", "Prostate: 37", "Skin Not Sun Exposed: 31",
-                    "Skin Sun Exposed: 114", "Spleen: 28", "Stomach: 76", 
-                    "Testis: 54", "Thyroid: 112", "Uterus: 32", "Vagina: 31")
 
-###--- Deng et al. 6 clusters ---###
+library(ggplot2)
+library(plotly)
+library(reshape2)
+library(cowplot)
+
+
+# make cell sample labels
+# want a version consistent with majority of the literature
+sample_labels <- read.table("../project/rdas/samples_id.txt", 
+                            header = TRUE, sep = " ",
+                            stringsAsFactors = FALSE)
+cell_labels <- vector("numeric", NROW(sample_labels))
+
+cell_labels[sample_labels$SMTSD == "Adipose - Subcutaneous"] <- "Adipose Subcutaneous"
+cell_labels[sample_labels$SMTSD == "Adipose - Visceral (Omentum)"] <- "Adipose Visceral"
+cell_labels[sample_labels$SMTSD == "Adrenal Gland"] <- "Adrenal Gland"
+cell_labels[sample_labels$SMTSD == "Artery - Aorta"] <- "Artery Aorta"
+cell_labels[sample_labels$SMTSD == "Artery - Coronary"] <- "Artery Coronary"
+cell_labels[sample_labels$SMTSD == "Artery - Tibial"] <- "Artery Tibial"
+cell_labels[sample_labels$SMTSD == "Bladder"] <- "Bladder"
+cell_labels[sample_labels$SMTSD == "Brain - Anterior cingulate cortex (BA24)"] <- "Brain Anterior Cingulate Cortex"
+cell_labels[sample_labels$SMTSD == "Brain - Amygdala"] <- "Brain Amygdala"
+cell_labels[sample_labels$SMTSD == "Brain - Caudate (basal ganglia)"] <- "Brain Caudate (basal ganglia)"
+cell_labels[sample_labels$SMTSD == "Brain - Cerebellar Hemisphere"] <- "Brain Cerebellar Hemisphere"
+cell_labels[sample_labels$SMTSD == "Brain - Cerebellum"] <- "Brain Cerebellum"
+cell_labels[sample_labels$SMTSD == "Brain - Cortex"] <- "Brain Cortex"
+cell_labels[sample_labels$SMTSD == "Brain - Frontal Cortex (BA9)"] <- "Brain Frontal Cortex"
+cell_labels[sample_labels$SMTSD == "Brain - Hippocampus"] <- "Brain Hippocampus"
+cell_labels[sample_labels$SMTSD == "Brain - Hypothalamus"] <- "Brain Hypothalamus"
+cell_labels[sample_labels$SMTSD == "Brain - Nucleus accumbens (basal ganglia)"] <- "Brain Nucleus Accumbens (basal ganglia)"
+cell_labels[sample_labels$SMTSD == "Brain - Putamen (basal ganglia)"] <- "Brain Putamen (basal ganglia)"
+cell_labels[sample_labels$SMTSD == "Brain - Spinal cord (cervical c-1)"] <- "Brain Spinal cord"
+cell_labels[sample_labels$SMTSD == "Brain - Substantia nigra"] <- "Brain - Substantia Nigr"
+cell_labels[sample_labels$SMTSD == "Breast - Mammary Tissue"] <- "Breast"
+cell_labels[sample_labels$SMTSD == "Cells - EBV-transformed lymphocytes"] <- "Cells Line EBV Lymphocytes"
+cell_labels[sample_labels$SMTSD == "Cells - Transformed fibroblasts"] <- "Cells Transformed Fibroblasts"
+cell_labels[sample_labels$SMTSD == "Cervix - Ectocervix"] <- "Cervix Ectocervix"
+cell_labels[sample_labels$SMTSD == "Cervix - Endocervix"] <- "Cervix Endocervix"
+cell_labels[sample_labels$SMTSD == "Colon - Sigmoid"] <- "Colon Sigmoid"
+cell_labels[sample_labels$SMTSD == "Colon - Transverse"] <- "Colon Transverse"
+cell_labels[sample_labels$SMTSD == "Esophagus - Gastroesophageal Junction"] <- "Esophagus Gastroesophageal Junction"
+cell_labels[sample_labels$SMTSD == "Esophagus - Mucosa"] <- "Esophagus Mucosa"
+cell_labels[sample_labels$SMTSD == "Esophagus - Muscularis"] <- "Esophagus Muscularis"
+cell_labels[sample_labels$SMTSD == "Fallopian Tube"] <- "Fallopian Tube"
+cell_labels[sample_labels$SMTSD == "Heart - Atrial Appendage"] <- "Heart Atrial Appendage"
+cell_labels[sample_labels$SMTSD == "Heart - Left Ventricle"] <- "Heart Left Ventricle"
+cell_labels[sample_labels$SMTSD == "Kidney - Cortex"] <- "Kidney Cortex"
+cell_labels[sample_labels$SMTSD == "Liver"] <- "Liver"
+cell_labels[sample_labels$SMTSD == "Lung"] <- "Lung"
+cell_labels[sample_labels$SMTSD == "Minor Salivary Gland"] <- "Minor Salivary Gland"
+cell_labels[sample_labels$SMTSD == "Muscle - Skeletal"] <- "Muscle Skeletal"
+cell_labels[sample_labels$SMTSD == "Nerve - Tibial"] <- "Nerve Tibial"
+cell_labels[sample_labels$SMTSD == "Ovary"] <- "Ovary"
+cell_labels[sample_labels$SMTSD == "Pancreas"] <- "Pancreas"
+cell_labels[sample_labels$SMTSD == "Pituitary"] <- "Pituitary"
+cell_labels[sample_labels$SMTSD == "Prostate"] <- "Prostate"
+cell_labels[sample_labels$SMTSD == "Skin - Not Sun Exposed (Suprapubic)"] <- "Skin Not Sun Exposed"
+cell_labels[sample_labels$SMTSD == "Skin - Sun Exposed (Lower leg)"] <- "Skin Sun Exposed"
+cell_labels[sample_labels$SMTSD == "Small Intestine - Terminal Ileum"] <- "Small Intestine Terminal Ileum"
+cell_labels[sample_labels$SMTSD == "Spleen"] <- "Spleen"
+cell_labels[sample_labels$SMTSD == "Stomach"] <- "Stomach"
+cell_labels[sample_labels$SMTSD == "Testis"] <- "Testis"
+cell_labels[sample_labels$SMTSD == "Thyroid"] <- "Thyroid"
+cell_labels[sample_labels$SMTSD == "Uterus"] <- "Uterus"
+cell_labels[sample_labels$SMTSD == "Vagina"] <- "Vagina"
+cell_labels[sample_labels$SMTSD == "Whole Blood"] <- "Whole Blood"
+
+# assign tissue labels
+rownames(omega) <- paste(cell_labels, 1:length(cell_labels), sep = "_")
+cell_labels_fact <- factor(cell_labels, 
+                           levels = sort(unique(cell_labels)) )
+
+
+# find tissue orders using hierarchical clustering
+# this is needs to be done before re-ordering the data
+docweights_per_family_mean <- apply(omega, 2, function(x) {
+    tapply(x, cell_labels, mean) })
+family_ordering <- heatmap(docweights_per_family_mean)$rowInd
+levels(cell_labels_fact) <- levels(cell_labels_fact)[family_ordering]
+
+
+# make the re-ordered dataframe
+df_ord <- do.call(rbind,
+                  lapply(1:nlevels(cell_labels_fact), function(ii) {
+                      temp_label <- rev(levels(cell_labels_fact))[ii]
+                      temp_df <- omega[which(cell_labels == temp_label), ]
+                      
+                      # find the dominant cluster in each sample
+                      each_sample_order <- apply(temp_df, 1, which.max)
+                      
+                      # find the dominant cluster across samples
+                      sample_order <- as.numeric(attr(table(each_sample_order), "name")[1])
+                      
+                      # reorder the matrix
+                      temp_df_ord <- temp_df[order(temp_df[ , sample_order]), ]
+                      
+                      temp_df_ord
+                  }) )
+dim(df_ord)
+
+# transform for ggplot2 input
+df_mlt <- reshape2::melt(t(df_ord))
+df_mlt <- plyr::rename(df_mlt, replace = c("Var1" = "topic",
+                                           "Var2" = "document"))
+
+# start making polar histogram
+source("R/polarHistogramStructure.R")
+
+# rename columns to satisfy function input
+df_plot <- df_mlt
+colnames(df_plot) <- c("score", "item", "value")
+df_plot$family <- sapply(df_mlt$document, function(x) {
+    strsplit(as.character(x), split= "_")[[1]][1]
+})
+#df_mlt$value <- df2$value*10000
+df_plot$family <- factor(df_plot$family)
+df_plot$score <- factor(df_plot$score)
+df_plot$item <- sapply(strsplit(as.character(df_plot$item), split = "_"), function(x) x[2])
+df_plot$item <- factor(df_plot$item)
+levels(df_plot$family) <- levels(cell_labels_fact)
+
+# define colors of the clusers
+cols1 <- c(RColorBrewer::brewer.pal(9, "Set1"),
+          RColorBrewer::brewer.pal(12, "Set3"))
+
+cols2 <- c("red", "blue", "cornflowerblue", "black", "cyan", "darkblue",
+            "brown4", "burlywood", "darkgoldenrod1", "darkgray", "deepskyblue",
+            "darkkhaki", "firebrick", "darkorchid", "hotpink", "green",
+            "magenta", "yellow", "azure1", "azure4")
+
+source("R/polarHistogramStructure.R")
+
+# being preparing for computing on midway
+save(polarHistogramStructure,
+     cols1, cols2,
+     df_plot, file = "rdas/rda-for-midway.rda")
+
+# the following code was run on midway to make the plot
+# run large interactive job
+# sinteractive --partition=bigmem --constraint=256G --ntasks=1 --cpus-per-task=1 --mem-per-cpu=128000
+
+png(file = "main-figure-cols1.png",
+    height = 7, width = 7, units = "in", res = 300)
+polarHistogramStructure(df2, palette = cols1,
+                        outerRadius = 1.8,
+                        innerRadius = 0.3,
+                        familyLabelDistance = 2, # same metric as outerRadius
+                        binSize = .5,
+                        circleProportion = 0.97,
+                        familyLabels = TRUE)
+dev.off()
+
+png(file = "main-figure-cols2.png",
+    height = 7, width = 7, units = "in", res = 300)
+polarHistogramStructure(df2, palette = cols2,
+                        outerRadius = 1.8,
+                        innerRadius = 0.3,
+                        familyLabelDistance = 2, # same metric as outerRadius
+                        binSize = .5,
+                        circleProportion = 0.97,
+                        familyLabels = TRUE)
+dev.off()
+
+
+
+
+###------ Deng et al. 6 clusters ------###
 
 
 # load the previously analyzed results
@@ -77,12 +234,13 @@ theme_set(theme_bw(base_size = 12)) +
 
 value_ifl <- 10000
 ticks_number <- 6
+cols <- "Accent"
 # Use RColorBrewer color
 # http://bxhorn.com/rcolorbrewer-palettes/
 a <- ggplot(df_mlt, 
             aes(x = document, y = value*10000, fill = factor(topic)) ) + 
         xlab("Cell types") + ylab("") +
-        scale_fill_brewer(palette = "Accent") +
+        scale_fill_brewer(palette = cols) +
         theme(legend.position = "none",
               axis.text = element_text(size = 4),
               title = element_text(size = 6)) +
@@ -302,12 +460,13 @@ theme_set(theme_bw(base_size = 12)) +
 
 value_ifl <- 10000
 ticks_number <- 6
+cols <- "Set1"
 # Use RColorBrewer color
 # http://bxhorn.com/rcolorbrewer-palettes/
 a <- ggplot(df_mlt, 
             aes(x = document, y = value*10000, fill = factor(topic)) ) + 
     xlab("Cell types") + ylab("") +
-    scale_fill_brewer(palette = "Set1") +
+    scale_fill_brewer(palette = cols) +
     theme(legend.position = "none",
           axis.text = element_text(size = 4),
           title = element_text(size = 6)) +
