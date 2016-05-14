02f22242e2f0187092a29431bbb1679521b82edc
Kushal K Dey <kshldey@gmail.com>
2016-05-13 23:45:37 -0500

    plos gen author summary added

---
+++ project/R/matthew-website-chunk.R
@@ -0,0 +1,12 @@
+- "author_list": "KK Dey", "CJ Hsiao", "M Stephens"
+"title": "Clustering RNA-seq expression data using grade of membership model"
+"rest_of_citation": "http://biorxiv.org/content/early/2016/05/03/051631"
+"external_url": "http://biorxiv.org/content/early/2016/05/03/051631"
+"extra_links":
+  - "name": "R package - devel"
+"url": "https://github.com/kkdey/CountClust"
+  - "name": "R package - release"
+"url": "https://www.bioconductor.org/packages/3.3/bioc/html/CountClust.html"
+  - "name": "Github page"
+"url": "https://stephenslab.github.io/count-clustering/"
+
