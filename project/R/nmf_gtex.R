1dd5c4b8e5c75b70128a03f22b70eb6af4b09eae
Kushal K Dey <kshldey@gmail.com>
2016-09-09 15:58:26 -0500

    added nmf on gtex

--- project/R/nmf_gtex.R
+++ project/R/nmf_gtex.R
@@ -2,3 +2,10 @@
 
 #######  Non negative matrix factorization GTEx tissues ###############
 
+library(NMF)
+library(data.table)
+
+cis_expr_data <- data.frame(fread("../external_data/GTEX_V6/cis_gene_expression.txt"))
+matdata <- cis_expr_data[,-(1:2)];
+
+out <- nmf(matdata, rank=20)
