184682052b7e7a644647a0c93f4b9fb36ea298f6
Kushal K Dey <kshldey@gmail.com>
2016-09-15 00:14:12 -0500

    Supp figs 9, 10 and 11 dendogram + hierarchical maps

--- project/R/nmf_gtex.R
+++ project/R/nmf_gtex.R
@@ -7,5 +7,6 @@ library(data.table)
 
 cis_expr_data <- data.frame(fread("../external_data/GTEX_V6/cis_gene_expression.txt"))
 matdata <- cis_expr_data[,-(1:2)];
+voom_matdata <- limma::voom(matdata)$E;
 
 out <- nmf(matdata, rank=20)
