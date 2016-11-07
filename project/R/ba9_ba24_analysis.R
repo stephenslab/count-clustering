ec8fe386f47dcfef572085841da06dba595456f3
Kushal K Dey <kshldey@gmail.com>
2016-11-07 14:02:16 -0600

    updated the tables with GO annotatiins and changed fig 2

--- project/R/ba9_ba24_analysis.R
+++ project/R/ba9_ba24_analysis.R
@@ -48,3 +48,25 @@ CountClust::StructureGGplot(omega = omega,
                                              axis_ticks_lwd_x = .1,
                                              axis_label_size = 7,
                                              axis_label_face = "bold"))
+
+
+##############  list of extracted important genes for clusters  ######################
+
+gene_names <- ExtractTopFeatures(topic_clus$clust_3$theta, top_features = 50, method = "poisson", options = "min")
+
+gene_names_file <- as.vector(read.table("../external_data/GTEX_V6/gene_names_GTEX_V6.txt"))
+gene_names_extract <- as.character(gene_names_file[,1])
+gene_names_extract <- sapply(gene_names_extract, function(l) substring(l,1,15))
+top_genes_3 <- apply(gene_names, c(1,2), function(l) return(gene_names_extract[l]))
+
+save(top_genes_3, file="../rdas/ba_gtex_top_genes_clus_3.rda")
+
+
+gene_names <- ExtractTopFeatures(topic_clus$clust_2$theta, top_features = 50, method = "poisson", options = "min")
+
+gene_names_file <- as.vector(read.table("../external_data/GTEX_V6/gene_names_GTEX_V6.txt"))
+gene_names_extract <- as.character(gene_names_file[,1])
+gene_names_extract <- sapply(gene_names_extract, function(l) substring(l,1,15))
+top_genes_2 <- apply(gene_names, c(1,2), function(l) return(gene_names_extract[l]))
+
+save(top_genes_2, file="../rdas/ba_gtex_top_genes_clus_2.rda")
--- project/R/dendextend_example.R
+++ project/R/dendextend_example.R
@@ -106,7 +106,11 @@ celltype_labels <-   factor(celltype_labels, levels = c("zy",
 
 numtypes <- length(unique(celltype_labels))
 library(colorspace) # get nice colors
-species_col <- rev(rainbow_hcl(numtypes))[as.numeric(celltype_labels)]
+cols <- c( rev(c("darkblue", "blue", "cornflowerblue", "cadetblue2")),
+           rev(c("darkgreen", "darkolivegreen4", "darkolivegreen3")),
+           rev(c("coral4", "coral3", "coral")) )
+
+species_col <- cols[as.numeric(celltype_labels)]
 
 d_deng <- dist(t(voom_counts)) # method="man" # is a bit better
 hc_deng <- hclust(d_deng, method = "complete")
@@ -121,7 +125,7 @@ dend <- as.dendrogram(hc_deng)
 # order it the closest we can to the order of the observations:
 #dend <- rotate(dend, 1:259)
 
-dend <- dend %>% set("leaves_pch", c(15)) %>% set("leaves_cex", c(1)) %>% set("leaves_col", species_col_adjusted)
+dend <- dend %>% set("leaves_pch", c(15)) %>% set("leaves_cex", c(2)) %>% set("leaves_col", species_col_adjusted)
 
 #labels(dend) <- rep("", length(species_col))
 # Color the branches based on the clusters:
@@ -155,7 +159,8 @@ dev.off()
 pdf("../plots/dendextend_deng_circle.pdf")
 par(mar = c(3,3,3,3))
 circlize_dendrogram(dend, labels=FALSE)
-legend("topleft", legend = deng_celltypes, fill = rev(rainbow_hcl(numtypes)))
+#legend("topleft", legend = deng_celltypes, 
+#       fill = cols)
 dev.off()
 
 
@@ -174,7 +179,14 @@ voom_counts <- limma::voom(counts)$E;
 celltype_labels <- factor(meta_data$tissue_subtype)
 numtypes <- length(unique(celltype_labels))
 library(colorspace) # get nice colors
-species_col <- rev(rainbow_hcl(numtypes))[as.numeric(celltype_labels)]
+
+library(RColorBrewer)
+# make color scheme
+col_vector <- c(RColorBrewer::brewer.pal(8, "Accent"),
+                RColorBrewer::brewer.pal(8, "Dark2")[1:4],
+                RColorBrewer::brewer.pal(12, "Paired")[1])
+
+species_col <- col_vector[as.numeric(celltype_labels)]
 
 #d_gtex_brain <- dist(t(voom_counts)) # method="man" # is a bit better
 #hc_gtex_brain <- hclust(d_gtex_brain, method = "complete")
@@ -203,7 +215,7 @@ dend <- as.dendrogram(hc_gtex_brain)
 #                      "(",labels(dend),")",
 #                      sep = "")
 
-dend <- dend %>% set("leaves_pch", c(15)) %>% set("leaves_cex", c(1)) %>% set("leaves_col", species_col_adjusted)
+dend <- dend %>% set("leaves_pch", c(15)) %>% set("leaves_cex", c(2)) %>% set("leaves_col", species_col_adjusted)
 
 #labels(dend) <- rep("",length(species_col))
 
@@ -219,29 +231,36 @@ plot(dend,
      main = "Clustered GTEx Brain data set
      (Labels give true tissue states)",
      horiz =  TRUE,  nodePar = list(cex = .007))
-legend("topleft", legend = gtex_brain_celltypes, fill = rainbow_hcl(numtypes))
+legend("topleft", legend = gtex_brain_celltypes, fill = rev(col_vector))
 dev.off()
 
 pdf("../plots/dendextend_gtex_brain_circle.pdf")
 par(mar = c(3,3,3,3))
 circlize_dendrogram(dend, labels=FALSE)
-legend("topleft", legend = gtex_brain_celltypes,
-       fill = rainbow_hcl(numtypes), cex=0.6)
+#legend("topleft", legend = gtex_brain_celltypes,
+#       fill = rev(col_vector), cex=0.6)
 dev.off()
 
 
 
 ###############   dendextend  GTEx whole tissues  #####################
-library(data.table)
-cis_data <- data.frame(fread("../external_data/GTEX_V6/cis_gene_expression.txt"))
+
+cis_data <- data.frame(data.table::fread("../external_data/GTEX_V6/cis_gene_expression.txt"))
 samples_id <- read.table("../external_data/GTEX_V6/samples_id.txt")
 tissue_labels <- factor(samples_id[,3])
 numtypes <- length(unique(tissue_labels))
 
-color = grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = T)]
-color1 <- sample(color, 53)
+#color = grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = T)]
+#color1 <- sample(color, 53)
 
-species_col <- rev(color1)[as.numeric(tissue_labels)]
+library(RColorBrewer)
+n <- 53
+qual_col_pals <- brewer.pal.info[brewer.pal.info$category == 'qual',]
+col_vector <- unlist(mapply(brewer.pal, 
+                            qual_col_pals$maxcolors, rownames(qual_col_pals)))
+
+
+species_col <- rev(col_vector)[as.numeric(tissue_labels)]
 
 
 hc_gtex <- get(load("../rdas/hclust_gtex.rda"))
@@ -267,7 +286,7 @@ dend <- rotate(dend, 1:8555)
 #                      "(",labels(dend),")",
 #                      sep = "")
 
-dend <- dend %>% set("leaves_pch", c(15)) %>% set("leaves_cex", c(1)) %>%
+dend <- dend %>% set("leaves_pch", c(15)) %>% set("leaves_cex", c(2)) %>%
     set("leaves_col", species_col_adjusted)
 
 #labels(dend) <- rep("",length(species_col))
@@ -291,6 +310,6 @@ dev.off()
 pdf("../plots/dendextend_gtex_circle.pdf")
 par(mar = c(3,3,3,3))
 circlize_dendrogram(dend, labels=FALSE)
-legend("topleft", legend = gtex_celltypes, 
-       fill = color1[1:numtypes], ncol=2, cex=0.5)
+#legend("topleft", legend = rev(gtex_celltypes), 
+#       fill = col_vector[1:numtypes], ncol=2, cex=0.5)
 dev.off()
